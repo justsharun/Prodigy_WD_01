0a662e7c055bfd45cb012d33eb55c2ff3b6ee68d
justsharun <justsharun27@gmail.com>
2024-07-06 18:29:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = '#555';
+        } else {
+            navbar.style.backgroundColor = '#333';
+        }
+    });
+});
